0164e3351668c908fde8f5b8b5fe27175907ddac
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-10-19 16:02:34 +0530

    Works

---
+++ Roots of functions/bisection.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+# Just change the function to whatever you need to find the root of
+def fx(x):
+    return x*np.log10(x) - 1.2 
+
+# Performs bisection method
+def bisection_method(a, b, accuracy):
+    i = 0
+    x = []
+    while True:
+        print("ITERATION " + str(i+1))
+        print("a = " + str(a) + " , b = ", str(b))
+        x.append((a+b)/2)
+        xval = 'x' + str(i) + '='
+        print()
+        print(xval, end='')
+        print(x[i])
+        print()
+        fx_val = fx(x[i])
+        fx_str = "f(x" + str(i) + ")="
+        print(fx_str, end='')
+        print(fx_val)
+        if fx_val > 0:
+            b = x[i]
+        else:
+            a = x[i]
+        if i > 0:
+
+            if int(x[i] * 10**accuracy) == int(x[i-1]* 10**accuracy):
+                final_str = "Since xn and xn-1 are same upto 4 decimal places, we have final x as:"
+                print(final_str , end='')
+                print(x[i])
+                break
+        
+        print()
+        print()
+        i += 1
+    return
+        
+
+def main():
+
+    # Inputs initial approximation
+    a = float(input("Enter the initial lower approx:"))
+    b = float(input("Enter the initial upper approx:"))
+    accuracy = int(input("Accuracy(correct upto how many digits after decimal point):"))
+
+    print()
+    print()
+
+    bisection_method(a,b,accuracy)
+
+if __name__=="__main__":
+    main()
